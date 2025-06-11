3087b8aae2e4badfbd7b13722fc6b37e9fc123e0
muhammad7984 <usmanullah5219@gmail.com>
2025-06-11 13:34:36 -0400

    update models  admin views urls and populate

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
+        path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
