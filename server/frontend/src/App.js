968e7061e639762bf56bc66a0be84659d3cb899f
muhammad7984 <usmanullah5219@gmail.com>
2025-06-13 13:16:26 -0400

    update urls and App

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; // ✅ Import Register
-
+import Dealers from './components/Dealers/Dealers';
+import PostReview from "./components/Dealers/PostReview"
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} /> {/* ✅ Add this */}
+       <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealer/:id" element={<Dealer/>} />
+        <Route path="/postreview/:id" element={<PostReview/>} />
+
+
     </Routes>
   );
 }
